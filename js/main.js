b615ba05aaa6c14780732c2e255240ddcf9a8eac
Amixir <alejopq639@gmail.com>
2021-11-27 20:54:53 -0500

    java 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ class registro{
 		this.usuario=usuarios;
 		this.contraseña=contraseñas;
 	}
-	
+}
+class verificacion {
+	showMessage(){
+		
+	}
 }
 
-
+//eventos al dar click
+window.onload=function(){
+document.getElementById('ingreso-form').addEventListener('submit',function(){
+	document.getElementById('usuarios').value
+	console.log(document.getElementById('usuarios').value)
+})
+}
