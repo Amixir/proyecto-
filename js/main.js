bcaacb529be7af4e9e4d5fc9d59a9b87a4dc8e9f
Amixir <alejopq639@gmail.com>
2021-11-27 22:44:31 -0500

    mensaje de error

--- js/main.js
+++ js/main.js
@@ -6,22 +6,46 @@ class registro{
 }
 class verificacion {
 	
-	mostrarmensaje(message, cssClass){
-		const mesa = (document.createElement('div'));
-		mesa.className ='alert alert-${cssClass}';
-		mesa.appendChild(document.createTextNode(message));
+	veri(usu){
+		if (usu.nombre === 'diego'){
+			alert('diego hola');
+		}
+		
 		
 	}
 	
+	
+	usuarioerror(){
+		
+	}
+	
+	mostrarmensaje(message, cssClass){
+		const mesa = (document.createElement('div'));
+		mesa.className ='alert alert-${cssClass} mt-4';
+		mesa.appendChild(document.createTextNode(message));
+		const contenedor = (document.querySelector('.contenedor'));
+		const aplicacion =document.querySelector('#aplicacion');
+		contenedor.insertBefore(mesa,aplicacion);
+		setTimeout(function(){
+			document.querySelector('.alert').remove();
+	}, 3000);
+	
+	}
 }
 
 //eventos al dar click
 window.onload=function(){
-document.getElementById('ingreso-form').addEventListener('submit',function(){
+document.getElementById('ingreso-form').addEventListener('submit',function(e){
 	const nombre = document.getElementById('usuarios').value;
 	const secret = document.getElementById('contraseñas').value;
 	console.log(nombre,secret);
 	
 	
+	
+	const usu = new registro(nombre,secret);
+	const ui = new verificacion();
+	ui.mostrarmensaje('como sea');
+	
+	e.preventDefault();
 })
 }
