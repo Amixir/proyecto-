de0368a58aee53ef38dbccab619920b658b236e6
Amixir <alejopq639@gmail.com>
2021-12-01 20:50:58 -0500

    java datos

---
+++ js/datos.js
@@ -0,0 +1,18 @@
+class Datos{
+    constructor(Contenedor,UCR,Cantidad){
+        this.contenedor = contenedor;
+        this.UCR = UCR;
+        this.Cantidad = cantidaducr;
+    }
+}
+class listas{
+    ingresardatos(){
+
+    }
+
+    mostrarmensajes(){
+
+    }
+}
+
+document.getElementById("contenedor-form").
