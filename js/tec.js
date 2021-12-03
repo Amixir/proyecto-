0f2072d1feb8ad5d3c8afb13e25d8995520201af
Amixir <alejopq639@gmail.com>
2021-12-02 21:36:12 -0500

    casi

--- js/tec.js
+++ js/tec.js
@@ -5,21 +5,42 @@ class datos {
         this.Cantidad = Cantidad;
     }
 }
-class listas{
-    ingresardatos(datosc){
-        const dato = (document.getElementById("lista-datos"));
-        const element = document.createElement("div");
-        element.innerHTML = `
-        <div class="card text-center mb-4">
-            <div class ="card body">
-                <strong>datosc</strong>: ${datosc.Contenedor}
-                <strong>datosuc</strong>: ${datosc.ucr}
-                <strong>datosca</strong>: ${datosc.Cantidad}
+
+class coduc {
+    constructor(vehiculo,cedu){
+        this.vehiculo = vehiculo;
+        this.cedu = cedu;
+    }
+}
+class UI {
+    ingresardatosc(datosc){
+        const listado = document.getElementById('lista-datos');
+        const elemento = document.createElement('div');
+        elemento.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class ="card body">
+                    <strong>Contenedor</strong>: ${datosc.Contenedor}
+                    <strong>Codigo UCR</strong>: ${datosc.ucr}
+                    <strong>Cantidad UCR</strong>: ${datosc.Cantidad}
+                 </div>
             </div>
-        </div>
         `;
-        datosc.appendChild(element);
+     listado.appendChild(elemento);
     
+    }
+
+    ingresardatosv(datosv){
+        const listada = document.getElementById('lista-datos');
+        const elementa = document.createElement('div');
+        elementa.innerHTML = `
+            <div class="card text-center mb-4">
+                <div class ="card body">
+                    <strong>Placa Vehiculo</strong>: ${datosv.vehiculo}
+                    <strong>Cedula conductor</strong>: ${datosv.cedu}
+                </div>
+            </div>
+        `;
+     listada.appendChild(elementa);
     }
 
     mostrarmensajes(){
@@ -37,10 +58,13 @@ document.getElementById('contenedor-form').addEventListener('submit', function(e
     const Cantidad = document.getElementById("cantidaducr").value
     console.log(Contenedor, ucr, Cantidad);
 
-    const datosc = new datos(Contenedor,ucr,Cantidad);
-    const dat = new listas();
-    dat.ingresardatos(datosc);
+    const datosc = new datos (Contenedor,ucr,Cantidad);
+    const dat = new UI ();
+    dat.ingresardatosc(datosc);
+    e.preventDefault()
 
+
+   
 
 // if para  revisar si esta fallando la declaracionde una varible
 //     var str,
@@ -56,5 +80,16 @@ document.getElementById('contenedor-form').addEventListener('submit', function(e
     
 
     // ESTO HAY QUE QUITARLO CUANDO SE AGREGUE EL SERVIDOR
+    
+})
+
+document.getElementById('transporte-form').addEventListener('submit', function(e){
+    const vehi = document.getElementById("PLACA").value
+    const cc = document.getElementById("cedula").value
+    console.log(vehi, cc);
+
+    const datosv = new  coduc (vehi, cc);
+    const data = new UI ();
+    data.ingresardatosv(datosv);
     e.preventDefault()
 })
