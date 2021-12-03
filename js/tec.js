ac86a1a94d462ea0480dadacf2d5aad1b8565081
Amixir <alejopq639@gmail.com>
2021-12-02 19:52:44 -0500

    recopilacion de datos

--- js/tec.js
+++ js/tec.js
@@ -1,9 +1,8 @@
-console.log("hola")
-class product {
-    constructor(Contenedor,UCR,Cantidad){
-        this.Contenedor = contenedor;
-        this.UCR = UCR;
-        this.Cantidad = cantidaducr;
+class datos {
+    constructor(Contenedor,ucr,Cantidad){
+        this.Contenedor = Contenedor;
+        this.ucr = ucr;
+        this.Cantidad = Cantidad;
     }
 }
 class listas{
@@ -14,8 +13,29 @@ class listas{
     mostrarmensajes(){
 
     }
+
+    borrardatos(){
+
+    }
 }
 
-document.getElementById('contenedor-form').addEventListener('submit', function(){
-    console.log(document.getElementById("contenedor").value)
+document.getElementById('contenedor-form').addEventListener('submit', function(e){
+    const Contenedor = document.getElementById("contenedor").value
+    const ucr = document.getElementById("unidadcarga").value
+    const Cantidad = document.getElementById("cantidaducr").value
+    console.log(Contenedor, ucr, Cantidad);
+
+    var str,
+element = document.getElementById('contenedor');
+if (element != null) {
+    str = element.value;
+}
+else {
+    str = null;
+}
+console.log(str);
+
+    console.log(new datos(Contenedor,ucr,Cantidad));
+    // ESTO HAY QUE QUITARLO CUANDO SE AGREGUE EL SERVIDOR
+    e.preventDefault()
 })
