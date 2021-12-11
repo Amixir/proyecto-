0f37ccc676fb72010c3430020884c8dbba8a1c3d
Amixir <alejopq639@gmail.com>
2021-12-11 00:41:28 -0500

    final

--- index.js
+++ index.js
@@ -3,23 +3,17 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 
+
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 mongoose.connect("mongodb://localhost/Proyecto");
 
-// const Ingreso = mongoose.model("Usuarios",{
-// });
+const Ingreso = mongoose.model("Uusuarioss",{
+    nuevo:String,
+    clave:Number,
+    });
 
-// const Vehiculos = mongoose.model("Vehiculos",{
-//     placa:String,
-//     tipo:String
-// });
 
-// const Contenedor = mongoose.model("Contenedor",{
-//     contenedor:String,
-//     cantidad:Number,
-//     cantidaducr:Number
-// });
 
 const Usuarios = mongoose.model("Usuarios",{
     nombre:String,
@@ -29,15 +23,15 @@ const Usuarios = mongoose.model("Usuarios",{
 
 // PAGINA DE INGRESO
 app.get("/ingreso",(req,res)=>{
-        let ingreso = `<form id='ingreso-form' class="card-body" action="CREAR VIAJE"method="post" >
+        let ingreso = `<form id='ingreso-form' class="card-body" action="/validar" method="post" >
         <h2 class="titulo">INGRESO </h2>
         <br>
-        <div class="form-groupp">
-        <input type="text" id="usuarios" placeholder="usuario" class="form.control"><br>
-        </div>
-        <div class="form-group">
-        <input type="password" id="contraseñas" placeholder="contraseña" class="password"><br><br>
-        </div>
+        <label for="nuevo">
+        <input type="text" name="nuevo" id="nuevo" placeholder="nombre" class="form.control"><br>
+        </label>
+        <label for="clave">
+        <input type="password" name="clave" id="clave" placeholder="contraseña" class="form.control"><br>
+        </label>
         <input type='submit' class="btn btn-lg btn-primary btn-block" value="Ingresar"><br>
         </form>
         <a href="/registro">
@@ -46,7 +40,41 @@ app.get("/ingreso",(req,res)=>{
         res.send(ingreso);
 });
 
+app.post ("/validar", (req,res)=>{    
+    new Ingreso({
+        nuevo:req.body.nuevo,
+        clave:req.body.clave,          
+    })
+    .save().then(()=>res.send(Ingreso));    
+    
+                let html = "";
+                        html+=`<span>"BIENVENIDO"</span><br>`;
+                        html+=`"<a href="/novedad">Continuar</a>"`;     
+                
+                res.send(html); });
 
+
+
+
+// app.post ("/validar", (req,res)=>{
+//        Usuarios.find((err,doc)=>{
+//         console.log(doc);
+//         let usuaria;
+//         for (var i in doc){
+//                 usuaria = doc[i];
+//                     html+=`<span>${usuario.nombre}</span>
+// //                 <span>${usuario.dependencia}</span> <span>${usuario.contraseña}</span> | `;
+//                 html+=`<a href="/borrar?id=${usuario._id}">Borrar</a><br>`;
+//         }        
+//             res.send(html);
+
+    // new Usuarios({
+    //     nombre:req.body.nombre,
+    //     contraseña:req.body.contraseña        
+    // }).save().then(()=>res.send(Usuarios));
+// };
+// });
+// });
 
 app.get("/registro",(req,res)=>{
     let formulario = `
@@ -68,10 +96,8 @@ app.get("/registro",(req,res)=>{
     res.send(formulario);
 
 });
-
-
 app.post ("/", (req,res)=>{
-    res.send("usuario guardado <a href=/listar>Continuar</a>");
+    res.send("usuario guardado <a href=/ingreso>Continuar</a>");
     new Usuarios({
         nombre:req.body.nombre,
         dependencia:req.body.dependencia,
@@ -80,184 +106,209 @@ app.post ("/", (req,res)=>{
 });
 
 
-// app.get("/borrar",(req,res)=>{       
-//         Usuarios.deleteOne({_id:req.query.id},(error)=>{
+// // app.get("/borrar",(req,res)=>{       
+// //         Usuarios.deleteOne({_id:req.query.id},(error)=>{
            
-//            let html ="";
-//             if(error){
-//                 res.send("error al borrar");
-//             }
-//             else{
-//                 res.send("usuario borrado <a href='/listar'>Volver</a> <a href='/novedad'>Continuar</a>");
-//             }
-//         })
-// });
+// //            let html ="";
+// //             if(error){
+// //                 res.send("error al borrar");
+// //             }
+// //             else{
+// //                 res.send("usuario borrado <a href='/listar'>Volver</a> <a href='/novedad'>Continuar</a>");
+// //             }
+// //         })
+// // });
 
-// app.get("/listar",(req,res)=>{
-//     Usuarios.find((err,doc)=>{
-//         console.log(doc);
-//         console.log(doc[0]);
-//         let html ="";
-//         let usuario;
-//         for (var i in doc){
-//                 usuario = doc[i];
-//                 html+=`<span>${usuario.nombre}</span>
-//                 <span>${usuario.dependencia}</span> <span>${usuario.contraseña}</span> | `;
-//                 html+=`<a href="/borrar?id=${usuario._id}">Borrar</a><br>`;
-//         }        
-//             res.send(html);
-//     });
-// });
-
-
+// // app.get("/listar",(req,res)=>{
+// //     Usuarios.find((err,doc)=>{
+// //         console.log(doc);
+// //         console.log(doc[0]);
+// //         let html ="";
+// //         let usuario;
+// //         for (var i in doc){
+// //                 usuario = doc[i];
+// //                 html+=`<span>${usuario.nombre}</span>
+// //                 <span>${usuario.dependencia}</span> <span>${usuario.contraseña}</span> | `;
+// //                 html+=`<a href="/borrar?id=${usuario._id}">Borrar</a><br>`;
+// //         }        
+// //             res.send(html);
+// //     });
+// // });
 
 
 
-// app.get("/novedad",(req,res)=>{
-//     let pagina = `
-//     <!DOCTYPE html>
-// <html lang="en">
 
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>GRUPO EXITO</title>
-//     <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
-// 	<link href="css/Contendor1.css" rel="stylesheet" />
-// 	</head>
-// 	<body>
-//     <nav class="navbar navbar-dark bg-dark">
-//         <a class="navbar-brand" href="#">GRUPO EXITO</a>
-//     </nav>
-// 	<div class="contenedores">
-//         	<div id="aplicacionP1" class="row pt-5">
-// 				<div class="row justify-content-start">
-//             <div class="col-4">
-//                 <div class="card">
-//                     <div class="card-header">
-// 						<div class ="col-auto p-5 text-center">
-// 							<div class="m-0 row justify-content-center">
-//                         		<h4>INGRESA LOS CONTENEDORES</h4>
-//                     				<form id="contenedor-form" class="card-body">
-//                         				<div class="form-group">
-//                             				<input type="text" id="contenedor" placeholder="CONTENEDOR" class="form-control"><br>
-// 										</div>
-// 											<div class="form-group">
-// 												<input type="number" id="unidadcarga" placeholder="CODIGO UCR" class="form-control" min="1" max="15"><br>
-// 											</div>
-// 											<div class="form-group">
-// 												<input type="number" id="cantidaducr" placeholder="CANTIDAD UCR" min="1" class="form-control"><br>
-// 											</div>
-// 											<input type="submit" id="boton" value="INGRESAR" text="center" class="btn btn-primary btn-block">
-// 											<!-- <a href="ucr.html">e
-// 											<input class="btn btn-primary btn-block" type="button" value="Continuar" id ="guardar"></a> -->
 
-// 									</form>
-// 									<a href="/reservas">
-// 									 <input type="submit" value="Guardar" text="center" class="btn btn-primary btn-block"></a>
-// 							</div>				
-//                         </div>            
-// 					</div>
-// 				</div>
-//             </div>
-			
-// 				<div class="col-4">
-// 					<div class="card">
-// 						<div class="card-header">
-// 							<div class ="col-auto p-5 text-center">
-// 								<div class="m-0 row justify-content-center">
-// 									<h4>INGRESA LOS DATOS DEL TRANSPORTADOR</h4>
-// 										<form id="transporte-form" class="card-body">
-// 											<div class="form-group">
-// 												<input type="text" id="PLACA" placeholder="PLACA DEL VEHICULO" class="form-control"><br>
-// 											</div>
-// 												<div class="form-group">
-// 													<input type="text" id="cedula" placeholder="CEDULA CONDUCTOR" class="form-control" ><br>
-// 												</div>
-// 												<input type="submit" value="INGRESAR" text="center" class="btn btn-primary btn-block">
+app.get("/novedad",(req,res)=>{
+    let pagina = `
+    <!DOCTYPE html>
+<html lang="en">
 
-// 												<!-- <a href="ucr.html">
-// 												<input class="btn btn-primary btn-block" type="button" value="Continuar" id ="guardar"></a> -->
-// 										</form>
-
-// 										</div>				
-// 							</div>            
-// 						</div>
-// 					</div>
-// 				</div>
-
-// 				<div id="lista-datos" class="col-4">
-// 				</div>
-// 				</div>
-// 			</div>
-// 		</div>
-// 		         <!-- PRODUCTS LIST -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GRUPO EXITO</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
+	<link href="css/Contendor1.css" rel="stylesheet" />
+	</head>
+	<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">GRUPO EXITO</a>
+    </nav>
+	<div class="contenedores">
+        	<div id="aplicacionP1" class="row pt-5">
+				<div class="row justify-content-start">
             
-//     </div>
+			
+				<div class="col-4">
+					<div class="card">
+						<div class="card-header">
+							<div class ="col-auto p-5 text-center">
+								<div class="m-0 row justify-content-center">
+									<h4>INGRESA LOS DATOS DEL TRANSPORTADOR</h4>
+                                    <form id="transporte-form" class="card-body" action="/transportador" method="post">
+                                        <label for="placa">
+                                        <input type="text" name="placa" id="placa" placeholder="placa del vehiculo" class="form.control"><br>
+                                        </label>
+                                        <label for="cedula">
+                                        <input type="text" name="cedula" id="cedula" placeholder="cedula del conductor" class="form.control"><br>
+                                        </label>
+												<input type="submit" value="INGRESAR" text="center" class="btn btn-primary btn-block">
 
-// 	<script src="js/tec.js"></script>
+												<!-- <a href="ucr.html">
+												<input class="btn btn-primary btn-block" type="button" value="Continuar" id ="guardar"></a> -->
+										</form>
+
+										</div>				
+							</div>            
+						</div>
+					</div>
+				</div>
+
+				<div id="lista-datos" class="col-4">
+				</div>
+				</div>
+			</div>
+		</div>
+		         <!-- PRODUCTS LIST -->
+            
+    </div>
+
+	<script src="js/tec.js"></script>
 	
-// 	</body>
-// 	</html>
-//     `;
-//     res.send(pagina);
-// });
-
-// app.get("/reservas",(req,res)=>{
-//     let reserva = `
-//     <!DOCTYPE html>
-// <html lang="en">
-// <head>
-// <meta charset="utf-8">
-// <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-// <title>GranDespacho</title>
-// <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
-// <link href="css/main12.css" rel="stylesheet" />
-
-// </head>
-// <body>
-// <nav>
-// <class="navbar navbar-expand-lg navbar-light bg-light>
-// <a href="https://www.exito.com/">Grupo Exito</a>
-// </nav>
-// <br>
-// <h1 class="titulo"> GranDespacho </h1>
-// </center>
-// <div class="contenedoringreso">
-// <div id="aplicacion" class="row pt-10">
-// <div class="m-0 row justify-content-center">
-// <div class ="col-auto p-5 text-center">
-// <div class="card">
-// <div class="card-header">	
-// </div>
-
-// <form  class="card-body" action="/grabar" method="post" >
-// <h2 class="titulo">INGRESO </h2>
-// <center>
-// <img src="img/logo.jpg" width="100" height="100"  >
-// </center>
-// <br>
-// <label for="nombre">
-// <input type="text" name="nombre" id="nombre" placeholder="nombre" class="form.control"><br>
-// </label>
-// <label for="dependencia">
-// <input type="text" name="dependencia" id="dependencia" placeholder="dependencia" class="form.control"><br>
-// </label>
-// <label for="contraseña">
-// <input type="password" name="contraseña" id="contraseña" placeholder="contraseña" class="form.control"><br>
-// </label>
-// <input type='submit' class="btn btn-lg btn-primary btn-block" value="Ingresar"><br>
-// </form>
-
-// </body>
-// </html>
-// `;
-// res.send(reserva);
-// });
+	</body>
+	</html>
+    `;
+    res.send(pagina);
+});
 
 
+const Vehiculos = mongoose.model("Vehiculos",{
+    placa:String,
+    cedula:String
+});
 
+app.post ("/transportador", (req,res)=>{
+    res.send("trasnportista guardado <a href=/contenedor>Continuar</a>");
+    new Vehiculos({
+        placa:req.body.placa,
+        cedula:req.body.cedula          
+    }).save().then(()=>res.send(Vehiculos));
+});
+
+app.get("/contenedor",(req,res)=>{
+    let contec = `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>GRUPO EXITO</title>
+        <link rel="stylesheet" href="https://bootswatch.com/5/zephyr/bootstrap.min.css">
+        <link href="css/Contendor1.css" rel="stylesheet" />
+        </head>
+        <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">GRUPO EXITO</a>
+        </nav>
+        <div class="contenedores">
+                <div id="aplicacionP1" class="row pt-5">
+                    <div class="row justify-content-start">
+                    <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+						<div class ="col-auto p-5 text-center">
+							<div class="m-0 row justify-content-center">
+                        		<h4>INGRESA LOS CONTENEDORES</h4>
+                    				<form id="contenedor-form" class="card-body" action="/contenedorfinal" method="post">
+                                    <label for="contenedor">
+                                    <input type="text" name="contenedor" id="contenedor" placeholder="digite contenedor" class="form.control"><br>
+                                    </label>
+                                    <label for="unidadcarga">
+                                    <input type="text" name="unidadcarga" id="unidadcarga" placeholder="tipo ucr" class="form.control"><br>
+                                    </label>
+                                    <label for="cantidaducr">
+                                    <input type="text" name="cantidaducr" id="cantidaducr" placeholder="cantidad UCR" class="form.control"><br>
+                                    </label>
+									<input type="submit" id="boton" value="INGRESAR" text="center" class="btn btn-primary btn-block">
+									</form>
+									</div>				
+                        </div>            
+					</div>
+				</div>
+            </div>`
+            ;
+res.send(contec);
+});
+
+const Contenedor = mongoose.model("Contenedor",{
+    contenedor:String,
+    cantidad:Number,
+    cantidaducr:Number
+});
+
+app.post ("/contenedorfinal", (req,res)=>{
+    res.send("contenedor guardado <a href=/final>Continuar</a>");
+    new Contenedor({
+        contenedor:req.body.contenedor,
+        unidadcarga:req.body.unidadcarga,
+        cantidaducr:req.body.cantidaducr
+    }).save().then(()=>res.send(Contenedor));
+});
+
+app.get("/final",(req,res)=>{
+    Vehiculos.find((err,doc)=>{
+        console.log(doc);
+        let html ="";
+        let carro;
+        for (var i in doc){
+                carro = doc[i];
+                html+=`<span>${carro.placa}</span>
+                <span>${carro.cedula}</span> | `;
+                html+=`<a href="/contemos">ver contenedores</a><br>`;
+        }        
+            res.send(html);
+    });
+});
+
+
+app.get("/contemos",(req,res)=>{
+    Contenedor.find((err,doc)=>{
+        console.log(doc);
+        let html ="";
+        let contenedora;
+        for (var i in doc){
+                contenedora = doc[i];
+                html+=`<span>${contenedora.contenedor}</span>
+                <span>${contenedora.unidadcarga}</span> <span>${contenedora.cantidaducr}</span> | `;
+                html+=`<a href="/novedad">volver para ingresar mas viajes</a><br>`;
+        }        
+            res.send(html);
+    });
+});
 app.listen(port, ()=>{
     console.log("empezo el servidor"); 
 });  
